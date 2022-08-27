4a49aa7790a3ef7d76a82f490a1c63fda53ae80a
Claudio Gallardo <claudiogallardo@Claudios-MacBook-Pro.local>
2022-08-26 22:54:37 -0700

    .gitignore

--- src/main/java/net/d3adlock/LogGenerator.java
+++ src/main/java/net/d3adlock/LogGenerator.java
@@ -9,36 +9,30 @@ import java.util.Random;
 
 public class LogGenerator {
 
-    private static BufferedWriter writer;
-
-    private static String ipAddress;
-    private static String name;
-    private static String city;
-    private static String country;
-    private static String latency;
-
     public static void main(String[] args) throws IOException {
         Faker faker = new Faker();
         Random rand = new Random();
 
         while(true) {
-
-            writer = new BufferedWriter(new FileWriter("mock.log"));
-            ipAddress = String.format("%s.%s.%s.%s",
+            BufferedWriter writer = new BufferedWriter(new FileWriter("mock.log"));
+            String ipAddress = String.format("%s.%s.%s.%s",
                     faker.number().numberBetween(140,240),
                     faker.number().numberBetween(140,240),
                     faker.number().numberBetween(140,240),
                     faker.number().numberBetween(140,240));
-            name = faker.name().fullName();
-            city = faker.address().city();
-            country = faker.address().country();
-            latency = String.format("%sms",faker.number().numberBetween(5,600));
+
+            String name = faker.name().fullName();
+            String city = faker.address().city();
+            String country = faker.address().country();
+            String latency = String.format("%sms",faker.number().numberBetween(5,600));
+
             writer.write(String.format("%s,%s,%s,%s\n",
                 ipAddress,
                 latency,
                 name,
                 city,
                 country));
+
             writer.close();
 
             try {
