1e9789aa8b5aedb25fe523fb9b4d5f72e19b9849
Claudio Gallardo <claudiogallardo@Claudios-MacBook-Pro.local>
2022-08-26 22:46:38 -0700

    generating IPs and latency

--- src/main/java/net/d3adlock/LogGenerator.java
+++ src/main/java/net/d3adlock/LogGenerator.java
@@ -2,21 +2,50 @@ package net.d3adlock;
 
 import com.github.javafaker.Faker;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
 public class LogGenerator {
-    public static void main(String[] args) {
-        
+
+    private static BufferedWriter writer;
+
+    private static String ipAddress;
+    private static String name;
+    private static String city;
+    private static String country;
+    private static String latency;
+
+    public static void main(String[] args) throws IOException {
         Faker faker = new Faker();
+        Random rand = new Random();
 
-        String streetName = faker.address().streetName();
-        String number = faker.address().buildingNumber();
-        String city = faker.address().city();
-        String country = faker.address().country();
+        while(true) {
 
-        System.out.println(String.format("%s\n%s\n%s\n%s",
-          number,
-          streetName,
-          city,
-          country));
+            writer = new BufferedWriter(new FileWriter("mock.log"));
+            ipAddress = String.format("%s.%s.%s.%s",
+                    faker.number().numberBetween(140,240),
+                    faker.number().numberBetween(140,240),
+                    faker.number().numberBetween(140,240),
+                    faker.number().numberBetween(140,240));
+            name = faker.name().fullName();
+            city = faker.address().city();
+            country = faker.address().country();
+            latency = String.format("%sms",faker.number().numberBetween(5,600));
+            writer.write(String.format("%s,%s,%s,%s\n",
+                ipAddress,
+                latency,
+                name,
+                city,
+                country));
+            writer.close();
 
+            try {
+                Thread.sleep(rand.nextInt(3) * 1000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
