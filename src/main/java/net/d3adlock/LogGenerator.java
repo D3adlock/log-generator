29ecc98a306f48292cd04c6f66e6c90dd9a17d7b
Claudio Gallardo <claudiogallardo@Claudios-MacBook-Pro.local>
2022-08-26 22:05:29 -0700

    adding faker

--- src/main/java/net/d3adlock/LogGenerator.java
+++ src/main/java/net/d3adlock/LogGenerator.java
@@ -1,7 +1,22 @@
 package net.d3adlock;
 
+import com.github.javafaker.Faker;
+
 public class LogGenerator {
     public static void main(String[] args) {
-        System.out.println("hello");
+        
+        Faker faker = new Faker();
+
+        String streetName = faker.address().streetName();
+        String number = faker.address().buildingNumber();
+        String city = faker.address().city();
+        String country = faker.address().country();
+
+        System.out.println(String.format("%s\n%s\n%s\n%s",
+          number,
+          streetName,
+          city,
+          country));
+
     }
 }
